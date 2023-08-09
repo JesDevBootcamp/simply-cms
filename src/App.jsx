d7cb816714f65a8158bff23ec69a0fa2cdd111d7
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-09 12:22:47 -0600

    Setup App routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
 // App: Component containing the entire application.
 
+import { Route, Routes } from "react-router-dom";
+
+import AdminLayout from "./layouts/AdminLayout";
+import PageLayout from "./layouts/PageLayout";
+
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import SettingsPage from "./pages/SettingsPage";
+import EditorPage from "./pages/EditorPage";
+
 export default function App() {
-	return <></>;
+	return (
+		<Routes>
+			<Route element={<AdminLayout />} path="/admin">
+				<Route index element={<LoginPage />} />
+				<Route path="/sign-up" element={<SignUpPage />} />
+				<Route path="/settings" element={<SettingsPage />} />
+				<Route path="/editor" element={<EditorPage />} />
+			</Route>
+			<Route element={<PageLayout />} path="*" />
+		</Routes>
+	);
 }
