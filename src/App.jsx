e6f0ae93e204c9cff3ec8f891b00d41dd4dea7ae
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-09 16:11:24 -0600

    Simplified App routes.

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,18 @@
 
 import { Route, Routes } from "react-router-dom";
 
-import AdminLayout from "./layouts/AdminLayout";
-import PageLayout from "./layouts/PageLayout";
-
+import SetupPage from "./pages/SetupPage";
 import LoginPage from "./pages/LoginPage";
-import SignUpPage from "./pages/SignUpPage";
 import EditorPage from "./pages/EditorPage";
+import WebPage from "./pages/WebPage";
 
 export default function App() {
 	return (
 		<Routes>
-			<Route element={<AdminLayout />} path="admin/">
-				<Route index element={<LoginPage />} />
-				<Route path="sign-up/" element={<SignUpPage />} />
-				<Route path="editor/" element={<EditorPage />} />
-			</Route>
-			<Route element={<PageLayout />} path="*" />
+			<Route path="/setup" element={<SetupPage />} />
+			<Route path="/login" element={<LoginPage />} />
+			<Route path="/editor" element={<EditorPage />} />
+			<Route element={<WebPage />} path="*" />
 		</Routes>
 	);
 }
