e20ce950710cd2e6c5cc910984a2d0f58e4a1eae
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-10 15:58:24 -0600

    Minified Welcome component.

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -3,15 +3,9 @@
 export default function Welcome({ title, subtitle }) {
 	return (
 		<header className="welcome">
-			<h1 className="welcome-title">
-				{title}
-			</h1>
-			{
-				subtitle &&
-				<h2 className="welcome-subtitle">
-					{subtitle}
-				</h2>
-			}
+			<h1 className="welcome-title">{title}</h1>
+			{subtitle &&
+			<h2 className="welcome-subtitle">{subtitle}</h2>}
 		</header>
 	);
 }
