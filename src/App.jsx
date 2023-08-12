f0c8db3969c46c6b90e2c29c048164c06b65ce0a
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-12 15:19:43 -0600

    Simplified Notes app react routes.

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,15 @@ import { Route, Routes } from "react-router-dom";
 import EditorPage from "./pages/EditorPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
-import WebPage from "./pages/WebPage";
 
 import "./styles/app.scss";
 
 export default function App() {
 	return (
 		<Routes>
-			<Route path="editor">
-				<Route index element={<EditorPage />} />
-				<Route path="login" element={<LoginPage />} />
-				<Route path="sign-up" element={<SignUpPage />} />
-			</Route>
-			<Route path=":slug" element={<WebPage />} />
+			<Route index element={<LoginPage />} />
+			<Route path="sign-up" element={<SignUpPage />} />
+			<Route path="editor" element={<EditorPage />} />
 		</Routes>
 	);
 }
