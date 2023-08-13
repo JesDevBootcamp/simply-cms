1e99c96d78a5bdd0a736a33165ab9cb5de84a300
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-12 20:27:43 -0600

    Added navigate to login page if user not signed into editor.

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,8 +1,22 @@
-// Editor Page: The control panel or "editor" for settings and content.
+// Editor Page: Page for editing and creating notes.
+
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import "../styles/editor-page.scss";
 
 export default function EditorPage() {
+	// Navigate to login page if user not logged in:
+	const navigate = useNavigate();
+	useEffect(() => {
+		axios.get("/api/login/").then(response => {
+			if (response.data === false) {
+				navigate("/");
+			}
+		});
+	}, []);
+
 	return (
 		<div className="editor-page">
 			<div className="editor-page-preview">
