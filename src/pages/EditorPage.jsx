eb785b4bd516a1e0da24f5602ea91d6d9a39747f
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-16 12:07:03 -0600

    Replaced EditorPage useEffect hook with Logout component.

--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,27 +1,17 @@
 // Editor Page: Page for editing and creating notes.
 
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { Navigate } from "react-router-dom";
+
+import Logout from "../components/Logout";
 
 import "../styles/editor-page.scss";
 
 export default function EditorPage() {
-	// Navigate to login page if user not logged in:
-	const navigate = useNavigate();
-	useEffect(() => {
-		axios.get("/api/login/").then(response => {
-			if (response.data === false) {
-				navigate("/");
-			}
-		});
-	}, []);
-
 	return (
 		<div className="editor-page">
-			<div className="editor-page-preview">
-				Webpage preview goes here!
-			</div>
+			<Logout>
+				<Navigate to="/" replace={true} />
+			</Logout>
 		</div>
 	);
 }
