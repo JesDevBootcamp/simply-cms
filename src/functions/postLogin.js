6cc3362f3aff672831435cdc33f63a1277d61d2c
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-17 11:38:02 -0600

    Removed callbacks from postLogin function.

--- src/functions/postLogin.js
+++ src/functions/postLogin.js
@@ -1,16 +1,11 @@
-// Post Login: Sends a POST request and runs callback function on response.
+// Post Login: Sends a POST request and returns boolean.
 
 import axios from "axios";
 
-export default async function postLogin(email, password, valid, invalid) {
+export default async function postLogin(email, password) {
 	// Send POST request to verify login:
 	const { data } = await axios.post("/api/login/", { email, password });
 
-	// Run callback function based on data boolean:
-	if (valid && invalid) {
-		!!data ? valid() : invalid();
-	}
-
 	// Return response data boolean:
-	return data;
+	return JSON.parse(data);
 }
