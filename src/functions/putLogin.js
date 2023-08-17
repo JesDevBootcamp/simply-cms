21ad8ad0e9175879f7028b8c5b15fbf31738219e
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-17 11:39:59 -0600

    Removed callbacks from putLogin function.

--- src/functions/putLogin.js
+++ src/functions/putLogin.js
@@ -1,16 +1,11 @@
-// Put Login: Sends PUT request to add login information and runs callbacks.
+// Put Login: Sends PUT request to add login information and returns boolean.
 
 import axios from "axios";
 
-export default async function putLogin(email, password, success, failure) {
+export default async function putLogin(email, password) {
 	// Send PUT request to add login information:
 	const { data } = await axios.put("/api/login/", { email, password });
 
-	// Run callback function based on data boolean:
-	if (success && failure) {
-		!!data ? success() : failure();
-	}
-
 	// Return response data boolean:
-	return data;
+	return JSON.parse(data);
 }
