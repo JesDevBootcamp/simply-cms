99bd24219b1c06f22d03526ab2a35dd2aca3890e
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-18 13:02:50 -0600

    Added children prop to Heading component.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -2,13 +2,14 @@
 
 import "../styles/heading.scss";
 
-export default function Heading({ title, subtitle }) {
+export default function Heading({ title, subtitle, children }) {
 	return (
 		<header className="heading">
 			<h2 className="heading-title">{title}</h2>
 			{subtitle && (
 				<h3 className="heading-subtitle">{subtitle}</h3>
 			)}
+			{children}
 		</header>
 	);
 }
