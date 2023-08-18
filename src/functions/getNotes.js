e02a0809827c32c5131a793d139ac1c6f66bba65
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-18 11:24:08 -0600

    Fixed getNotes function.

--- src/functions/getNotes.js
+++ src/functions/getNotes.js
@@ -1,9 +1,11 @@
 // Get Notes: Sends a GET request and returns user note data.
 
+import axios from "axios";
+
 export default async function getNotes() {
 	// Send GET request for user notes:
 	const { data } = await axios.get("/api/notes/");
 
-	// Return response data:
-	return JSON.parse(data);
+	// Return response data array:
+	return data;
 }
