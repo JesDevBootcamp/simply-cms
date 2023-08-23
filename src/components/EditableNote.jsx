dcb1874b184ecf02f56625c84256845d5de61d26
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-23 12:10:41 -0600

    Fixed EditableNote component title updating.

--- src/components/EditableNote.jsx
+++ src/components/EditableNote.jsx
@@ -16,11 +16,19 @@ export default function EditableNote({ note, callback }) {
 
 	// Automatically save note and run callback on change:
 	useEffect(() => {
+		// Create timer for updating content on change:
 		const timer = setTimeout(async () => {
-			const title = content.substring(0, 60) || "No Content";
+			// Get title using max of 60 characters from first line:
+			let title = content.match(/.{0,60}/)[0] || "No Content";
+			// Truncate any overflowing title with ellipsis:
+			title = title.replace(/(.{59})./, "$1…");
+			// Update note in database using POST:
 			const post = await postNote(title, content, note.noteId);
+			// Run callback passing in updated note:
 			callback(post);
-		}, 350);
+		}, 500);
+
+		// Reset timer for updating content on change:
 		return () => {
 			clearTimeout(timer);
 		};
