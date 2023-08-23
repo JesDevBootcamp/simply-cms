ea1be5ee77814c37a97878e95398b639084cf5a8
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-23 15:27:58 -0600

    Added NoteControls component.

---
+++ src/components/NoteControls.jsx
@@ -0,0 +1,28 @@
+// Note Controls: Controls for creating and deleting notes.
+
+import Button from "./Button";
+
+import putNote from "../functions/putNote";
+import deleteNote from "../functions/deleteNote";
+
+import "../styles/note-controls.scss";
+
+export default function NoteControls({ note, callback }) {
+	// Handler for putting a note in database:
+	async function putHandler() {
+		callback(await putNote());
+	}
+
+	// Handler for deleting a note in database:
+	async function deleteHandler() {
+		await deleteNote(note.noteId);
+		callback(false);
+	}
+
+	return (
+		<div className="note-controls">
+			<Button action={putHandler}>Create</Button>
+			<Button action={deleteHandler}>Delete</Button>
+		</div>
+	);
+}
