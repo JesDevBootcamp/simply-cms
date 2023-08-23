6511c5f4fa92306e5f1689c512ce79cb086f3349
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-22 22:09:05 -0600

    Added EditableNote component.

---
+++ src/components/EditableNote.jsx
@@ -0,0 +1,36 @@
+// Editable Note: Simple editable note that autosaves.
+
+import { useEffect, useState } from "react";
+import DOMPurify from "dompurify";
+
+import postNote from "../functions/postNote";
+
+import "../styles/editable-note.scss";
+
+export default function EditableNote({ note, callback }) {
+	// Create state for note content:
+	const [content, setContent] = useState(note.content);
+
+	// Automatically save note and run callback on change:
+	useEffect(() => {
+		const timer = setTimeout(async () => {
+			const title = content.substring(0, 60) || "No Content";
+			const post = await postNote(title, content, note.noteId);
+			callback(post);
+		}, 350);
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [content]);
+
+	// Return sanitized note content:
+	return (
+		<textarea
+			className="editable-note"
+			value={content}
+			onChange={(event) => {
+				setContent(DOMPurify.sanitize(event.target.value));
+			}}
+		/>
+	);
+}
