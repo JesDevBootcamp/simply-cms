d984fe4176e67a76012c0c412de869412f890048
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-22 22:11:17 -0600

    Reduced RenderedNote component to only take in content.

--- src/components/RenderedNote.jsx
+++ src/components/RenderedNote.jsx
@@ -1,30 +1,16 @@
-// Rendered Note: Renders HTML from Markdown for any created note.
+// Rendered Note: Renders HTML from Markdown note content.
 
 import DOMPurify from "dompurify";
 import { marked } from "marked";
-import { useEffect, useState } from "react";
-
-import getNote from "../functions/getNote";
 
 import "../styles/rendered-note.scss";
 
-export default function RenderedNote({ id }) {
-	// Create state for Markdown content:
-	const [markdown, setMarkdown] = useState("");
-
-	// Update rendered content on ID change:
-	useEffect(() => {
-		(async() => {
-			const { content } = await getNote(id);
-			setMarkdown(content || "");
-		})();
-	}, [id]);
-
+export default function RenderedNote({ content }) {
 	// Return rendered sanitized Markdown note:
 	return (
-		<div className="rendered-note"
+		<article className="rendered-note"
 			dangerouslySetInnerHTML={{
-				__html: DOMPurify.sanitize(marked.parse(markdown))
+				__html: DOMPurify.sanitize(marked.parse(content))
 			}}
 		/>
 	);
