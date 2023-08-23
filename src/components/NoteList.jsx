a9c1b6dc854e041b364a662533f929ee4ef26d55
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-22 22:11:48 -0600

    Added NoteList component.

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,29 +1,15 @@
 // Note List: Scrollable list of all user notes.
 
-import { useState, useEffect } from "react";
-import getNotes from "../functions/getNotes";
-
-export default function NoteList({ callback }) {
-	// Create state for user notes:
-	const [notes, setNotes] = useState([]);
-
-	// Get array of all user notes:
-	useEffect(() => {
-		(async () => setNotes(await getNotes() || []))();
-	}, []);
-
+export default function NoteList({ list, callback }) {
 	return (
 		<ul className="note-list">
-			{notes.map(note =>
-				<li className="note-list-item" onClick={() => callback(note)} key={note.noteId}>
-					<h4 className="note-list-title">
-						{note.title}
-					</h4>
-					<span className="note-list-content">
-						{note.content.replace(/(.{100})..+/, "$1&hellip;")}
-					</span>
+			{list.map((note) => (
+				<li className="note-list-item"
+					onClick={() => callback(note)}
+					key={note.noteId}>
+					<strong>{note.title}</strong>
 				</li>
-			)}
+			))}
 		</ul>
 	);
 }
