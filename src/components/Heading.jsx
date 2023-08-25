fffb5290ac2306fe74e45c6d5d670fe4570cff44
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-25 09:28:08 -0600

    Made Heading component title optional.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -5,11 +5,15 @@ import "../styles/heading.scss";
 export default function Heading({ title, subtitle, children }) {
 	return (
 		<header className="heading">
-			<h2 className="heading-title">{title}</h2>
+			{title && (
+				<h2 className="heading-title">{title}</h2>
+			)}
 			{subtitle && (
 				<h3 className="heading-subtitle">{subtitle}</h3>
 			)}
-			{children}
+			<div className="heading-content">
+				{children}
+			</div>
 		</header>
 	);
 }
