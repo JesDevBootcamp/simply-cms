3a549c44c2bef48a0f7061a3fb01d6e283ec18ec
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-30 15:11:57 -0600

    Added styles for the Button component.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,10 @@
 
 import "../styles/button.scss";
 
-export default function Button({ action, type, children }) {
+export default function Button({ action, type, variant, children }) {
 	return (
 		<button
-			className="button"
+			className={["button", variant].join(" ")}
 			type={type}
 			onClick={action}>
 			{children}
