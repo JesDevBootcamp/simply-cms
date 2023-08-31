767707984caf5b001d50eee53e9142ba2f37f401
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-31 16:24:19 -0600

    Added active state to NoteList items.

--- src/components/NoteList.jsx
+++ src/components/NoteList.jsx
@@ -1,16 +1,24 @@
 // Note List: Scrollable list of all user notes.
 
+import { useState } from "react";
+
 import "../styles/note-list.scss";
 
 export default function NoteList({ list, callback }) {
+	// Create state for active note item:
+	const [active, setActive] = useState(list[0].noteId);
+
 	return (
 		<ul className="note-list">
 			{list.map((note) => (
-				<li className="note-list-item"
-					onClick={() => callback(note)}
-					onFocus={() => callback(note)}
+				<li
 					key={note.noteId}
-					tabIndex={0}>
+					tabIndex={0}
+					className={`note-list-item ${active == note.noteId ? "active" : ""}`}
+					onFocus={() => {
+						setActive(note.noteId);
+						callback(note);
+					}}>
 					<strong>{note.title}</strong>
 				</li>
 			))}
