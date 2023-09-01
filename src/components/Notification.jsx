a8cf7dc191ecd1a3427f018df328f46371aae28a
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-31 21:02:15 -0600

    Fixed Notification component.

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,24 +1,33 @@
 // Notification: Displays a dismissable popover notification.
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "../styles/notification.scss";
 
-export default function Notification({ message }) {
+export default function Notification({ message, callback, show = true }) {
 	// Create state for whether notification is open:
-	const [open, setOpen] = useState(true);
+	const [open, setOpen] = useState(show);
+
+	// Set open state when show property changes:
+	useEffect(() => setOpen(show), [show]);
+
+	// Create handler for closing notification: 
+	function closeHandler() {
+		setOpen(false);
+		callback(false);
+	}
 
 	return open && (
 		<article
 			className="notification"
-			onAnimationEnd={() => setOpen(false)}>
+			onAnimationEnd={closeHandler}>
 			<span
 				className="notification-message">
 				{message}
 			</span>
 			<button
 				className="notification-close"
-				onClick={() => setOpen(false)}>
+				onClick={closeHandler}>
 				&times;
 			</button>
 		</article>
