09800f4ec61154255d7ee373ea11d0d5d3837496
Joshua Sandoval <joshuaisgoogling@gmail.com>
2023-08-31 21:03:47 -0600

    Added alert notification to LoginPage when login fails.

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Heading from "../components/Heading";
 import TextField from "../components/TextField";
 import Button from "../components/Button";
+import Notification from "../components/Notification";
 
 import postLogin from "../functions/postLogin";
 import putLogin from "../functions/putLogin";
@@ -13,6 +14,9 @@ import useLogin from "../functions/useLogin"
 import "../styles/login-page.scss";
 
 export default function LoginPage() {
+	// Create state for login alert notification:
+	const [alert, setAlert] = useState(false);
+
 	// Create state for current login presence:
 	const [login, setLogin] = useLogin("editor");
 
@@ -25,8 +29,8 @@ export default function LoginPage() {
 		// Prevent default page reload:
 		event.preventDefault();
 
-		// POST login and set state if true:
-		await postLogin(email, password) && setLogin(true);
+		// POST login and set state if true, alert if false:
+		await postLogin(email, password) ? setLogin(true) : setAlert(true);
 
 		// Reset password state:
 		setPassword("");
@@ -41,7 +45,10 @@ export default function LoginPage() {
 		loginHandler(event);
 	}
 
-	return !login && (
+	return !login && <>
+		<Notification
+			message="Login authentication failed, try another email or password."
+			show={alert} callback={setAlert} />
 		<main className="login-page">
 			<Heading title="Simply Notes!" subtitle="Login or Sign-up:" />
 			<form className="login-page-form" onSubmit={loginHandler}>
@@ -67,5 +74,5 @@ export default function LoginPage() {
  				</fieldset>
 			</form>
 		</main>
-	);
+	</>;
 }
